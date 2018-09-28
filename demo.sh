d1284fcd1b7840910caa1f6d3ebebe189740b7b2
Chas Schley <cschley@Chass-MBP.hsd1.ca.comcast.net>
2018-09-28 08:32:57 -0700

    Shorten path to /ansible.

--- demo.sh
+++ demo.sh
@@ -6,7 +6,7 @@
 #   I have ec2.py installed under inventory/.
 #   A Dockerfile defines my ansible-playbook.
 #   Required by ec2.py: my EC2 API credentials are in my environment.
-#   EC2 machines are in the "modus-demo" group and tag. Inventory manages them by that.
+#   Inventory manages EC2 machines based on the "modus" group and tag.
 #
 
 # Build a Docker image to run ansible-playbook.
@@ -21,7 +21,7 @@ docker run --rm -it -v $(pwd) ansible-playbook --version
 docker run\
  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\
  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY\
- --rm -it -v $(pwd):/ansible/playbooks ansible-playbook provision-ec2.yml
+ --rm -it -v $(pwd):/ansible ansible-playbook provision-ec2.yml
 
 # Just checking...
 ansible -i inventory/ec2.py -u centos all -m ping
@@ -35,19 +35,19 @@ fi
 docker run\
  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\
  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY\
- --rm -it -v $(pwd):/ansible/playbooks ansible-playbook systemd-units.yml
+ --rm -it -v $(pwd):/ansible ansible-playbook systemd-units.yml
 
 # Do it again to check idempotency.
 docker run\
  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\
  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY\
- --rm -it -v $(pwd):/ansible/playbooks ansible-playbook systemd-units.yml
+ --rm -it -v $(pwd):/ansible ansible-playbook systemd-units.yml
 
 # Tear it all down.
 docker run\
  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID\
  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY\
- --rm -it -v $(pwd):/ansible/playbooks ansible-playbook terminate-ec2.yml
+ --rm -it -v $(pwd):/ansible ansible-playbook terminate-ec2.yml
 
 ###
 # DEBUGGING
@@ -55,7 +55,7 @@ docker run\
 # Run an ssh keypair while the playbook is playing.
 docker run --rm -it -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
     -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
-    -v $(pwd):/ansible/playbooks \
+    -v $(pwd):/ansible \
     ansible-playbook provision-ec2.yml
 
 #
